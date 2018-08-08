2a6f7c32dd1222f98af38aa93b49f1c5d7e7cf5e
swholm19 <swholm19@gmail.com>
2018-08-08 14:54:06 -0400

    Refactored Serializer Code add Validations
    -Refactored serializer method to have smaller functions and follow
    better design principals.
    -Implemented validations on certain attributes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :name, :expiration_date, presence: true
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -3,25 +3,24 @@ class ItemSerializer < ActiveModel::Serializer
   has_one :user
 
   def warning
-    if Date.today.month == 12
-      warning_date = Date.new(Date.today.year + 1, 1, Date.today.day)
-    else
-      warning_date = Date.new(Date.today.year, Date.today.month + 1, Date.today.day)
-    end
-
-    warr_year = object.expiration_date.year
-    warr_month = object.expiration_date.month
-    warr_day = object.expiration_date.day
-
-    # true means you need to send an email warning
-    if (warr_year - warning_date.year).positive?
-      return false
-    elsif (warr_year - warning_date.year).zero? && (warr_month - warning_date.month).positive?
-      return false
-    elsif (warr_year - warning_date.year).zero? && (warr_month - warning_date.month).zero? && (warr_day - warning_date.day).positive?
+    warning_date = calculate_warning_date
+    if (object.expiration_date.year - warning_date.year).positive? ||
+       ((object.expiration_date.year - warning_date.year).zero? &&
+       (object.expiration_date.month - warning_date.month).positive?) ||
+       ((object.expiration_date.year - warning_date.year).zero? &&
+       (object.expiration_date.month - warning_date.month).zero? &&
+       (object.expiration_date.day - warning_date.day).positive?)
       return false
     else
       return true
+    end
+  end
+
+  def calculate_warning_date
+    if Date.today.month == 12
+      Date.new(Date.today.year + 1, 1, Date.today.day)
+    else
+      Date.new(Date.today.year, Date.today.month + 1, Date.today.day)
     end
   end
 end
